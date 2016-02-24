9c61c53158df0689e2c1df94d736d142c98289c2
pbrun <pierre@mobizel.com>
2016-02-24 19:22:08 +0100

    [A] fix init script

--- phpback/scripts/run.sh
+++ phpback/scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -X POST -d "hostname=$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT&username=$MYSQL_USER&password=$MYSQL_PASSWORD&database=$MYSQL_NAME&adminname=$APP_ADMIN_EMAIL&adminemail=$APP_ADMIN_EMAIL&adminpass=$APP_ADMIN_PWD&adminrpass=$APP_ADMIN_PWD" http://127.0.0.1/install1.php --header "Content-Type:text/html"
-curl -X POST -d "title=$APP_TITLE&mainmail=$APP_ADMIN_EMAIL&rpublic=$APP_CAPTCHA_PUBLIC&rprivate=$APP_CAPTCHA_PRIVATE&smtp-host=$APP_SMTP_HOST&smtp-user=$APP_SMTP_USER&smtp-password=$APP_SMTP_PWD&smtp-port=$APP_SMTP_PORT" http://127.0.0.1/install2.php --header "Content-Type:text/html"
+mysql -h $MYSQL_PORT_3306_TCP_ADDR -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create database $MYSQL_NAME;"
+curl --data "hostname=$MYSQL_PORT_3306_TCP_ADDR&username=$MYSQL_USER&password=$MYSQL_PASSWORD&database=$MYSQL_NAME&adminname=$APP_ADMIN_EMAIL&adminemail=$APP_ADMIN_EMAIL&adminpass=$APP_ADMIN_PWD&adminrpass=$APP_ADMIN_PWD" http://127.0.0.1/install/install1.php
+curl --data "title=$APP_TITLE&mainmail=$APP_ADMIN_EMAIL&rpublic=$APP_CAPTCHA_PUBLIC&rprivate=$APP_CAPTCHA_PRIVATE&smtp-host=$APP_SMTP_HOST&smtp-user=$APP_SMTP_USER&smtp-password=$APP_SMTP_PWD&smtp-port=$APP_SMTP_PORT" http://127.0.0.1/install/install2.php
 
 rm -rf /var/www/install
